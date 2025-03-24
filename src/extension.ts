cb51de7e8551c5c235e7f9366eb72752b8c75882
Covei Rares <rarescovei5@gmail.com>
2025-03-24 08:58:46 +0200

    Created a button in the status bar that opens a webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,167 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// ---------------------- Extension Logic ----------------------
 export function activate(context: vscode.ExtensionContext) {
+  /**
+   * Create a button to open the webview for the extension
+   */
+  const sBarButton = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    0
+  );
+  sBarButton.text = '$(notebook-mimetype) Code Snippets';
+  sBarButton.command = 'principium-snippets.openWebview';
+  sBarButton.tooltip = 'Open Code Snippet Manager';
+  sBarButton.show();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "principium-snippets" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('principium-snippets.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Principium Snippets!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand('principium-snippets.openWebview', () => {
+      SnippetPanel.createOrShow(context.extensionUri);
+    })
+  );
+  context.subscriptions.push(sBarButton);
 }
-
-// This method is called when your extension is deactivated
 export function deactivate() {}
+//  ------------------------- Webview --------------------------
+
+class SnippetPanel {
+  /**
+   * Track the currently panel. Only allow a single panel to exist at a time.
+   */
+  public static currentPanel: SnippetPanel | undefined;
+
+  public static readonly viewType = 'snippetManager';
+
+  private readonly _panel: vscode.WebviewPanel;
+  private readonly _extensionUri: vscode.Uri;
+  private _disposables: vscode.Disposable[] = [];
+
+  private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    this._panel = panel;
+    this._extensionUri = extensionUri;
+
+    // Set the webview's initial html content
+    this._panel.webview.html = this._getHtmlForWebview(this._panel.webview);
+
+    // Listen for when the panel is disposed
+    // This happens when the user closes the panel or when the panel is closed programmatically
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+    // Handle messages from the webview
+    this._panel.webview.onDidReceiveMessage(
+      (message) => {
+        switch (message.command) {
+          case 'alert':
+            vscode.window.showErrorMessage(message.text);
+            return;
+        }
+      },
+      null,
+      this._disposables
+    );
+  }
+
+  public static createOrShow(extensionUri: vscode.Uri) {
+    const column = vscode.window.activeTextEditor
+      ? vscode.window.activeTextEditor.viewColumn
+      : undefined;
+
+    // If we already have a panel, show it.
+    if (SnippetPanel.currentPanel) {
+      SnippetPanel.currentPanel._panel.reveal(column);
+      return;
+    }
+
+    // Otherwise, create a new panel.
+    const panel = vscode.window.createWebviewPanel(
+      SnippetPanel.viewType,
+      'Snippet Manager',
+      column || vscode.ViewColumn.One,
+      getWebviewOptions(extensionUri)
+    );
+
+    SnippetPanel.currentPanel = new SnippetPanel(panel, extensionUri);
+  }
+
+  public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    SnippetPanel.currentPanel = new SnippetPanel(panel, extensionUri);
+  }
+
+  public doRefactor() {
+    // Send a message to the webview webview.
+    // You can send any JSON serializable data.
+    this._panel.webview.postMessage({ command: 'refactor' });
+  }
+
+  public dispose() {
+    SnippetPanel.currentPanel = undefined;
+
+    // Clean up our resources
+    this._panel.dispose();
+
+    while (this._disposables.length) {
+      const x = this._disposables.pop();
+      if (x) {
+        x.dispose();
+      }
+    }
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    // Uris
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js')
+    );
+    const stylesResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css')
+    );
+    const stylesMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css')
+    );
+
+    // Use a nonce to only allow specific scripts to be run
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+			<html lang="en">
+				<head>
+					<meta charset="UTF-8">
+					<!--
+						Use a content security policy to only allow loading images from https or from our extension directory,
+						and only allow scripts that have a specific nonce.
+					-->
+					<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
+
+					<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+					<link href="${stylesResetUri}" rel="stylesheet">
+					<link href="${stylesMainUri}" rel="stylesheet">
+
+					<title>Snipet Manager</title>
+				</head>
+				<body>
+					
+					<script nonce="${nonce}" src="${scriptUri}"></script>
+				</body>
+			</html>`;
+  }
+}
+//  -------------------- Utility Functions ---------------------
+function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
+  return {
+    // Enable javascript in the webview
+    enableScripts: true,
+
+    // And restrict the webview to only loading content from our extension's `media` directory.
+    localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')],
+  };
+}
+function getNonce() {
+  let text = '';
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
