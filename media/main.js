e638c001fe03c9162a71db65388f668e814d0db4
Covei Rares <rarescovei5@gmail.com>
2025-05-01 12:37:25 +0300

    Optimised Code

--- media/main.js
+++ media/main.js
@@ -1,107 +1,88 @@
 (function () {
-  // VSC - Extension API
+  // -=-=-=-=-=-=- VSC - Extension API -=-=-=-=-=-=-
   const vscode = acquireVsCodeApi();
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-  // Page Buttons
+  // -=-=-=-=-=-=- Init Dom Variables -=-=-=-=-=-=-
+  // Sidebar
   const settingsBtn = document.getElementById('settings-btn');
   const snippetsBtn = document.getElementById('snippets-btn');
-
-  // Add new Snippet Folder
   const newFolderBtn = document.getElementById('new-folder-btn');
 
-  // Handle changing the Page Content
-  const MainContent = document.querySelector('main');
+  // Snippets
+  const gridViewBtn = document.getElementById('gridView-btn');
+  const listViewBtn = document.getElementById('listView-btn');
+  const snippetsContainer = document.getElementById('snippets-container');
+
+  // General
+  const pages = {
+    snippetPage: document.getElementById('snippets-page'),
+    settingsPage: document.getElementById('settings-page'),
+  };
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+  // -=-=-=-=-=-=- Helper Functions For Handling Dom Changes -=-=-=-=-=-=-
+  const getSnippetsViewHTML = (viewType) => {
+    switch (viewType) {
+      case 'gridView-btn':
+        return '<h1>Grid</h1>';
+
+      case 'listView-btn':
+        return '<h1>List</h1>';
+    }
+  };
   const changePage = (path) => {
     switch (path) {
       case 'snippets-btn':
-        // Format the SnippetData into HTML
-        const getSnippetsViewHTML = (viewType) => {
-          switch (viewType) {
-            case 'gridView-btn':
-              return '<h1>Grid</h1>';
-
-            case 'listView-btn':
-              return '<h1>List</h1>';
-          }
-        };
-
-        //  Setup the static parts of the page early
-        MainContent.innerHTML = `
-          <div id="snippetsTop-search">
-            <input type='text' placeholder='Search Snippets...'/>
-            <div>
-              <button id='selected-language-btn'>All Languages</button>
-            </div>
-          </div>
-          <div id="snippetsMiddle-displayWay">
-            <button class="btn active" id="gridView-btn">
-              Grid View
-            </button>
-            <button class="btn" id="listView-btn">
-              List View
-            </button>
-          </div>
-          <div id="snippets-container"></div>`;
-
-        // Handle changing how the snippets are Displayed
-        const gridViewBtn = document.getElementById('gridView-btn');
-        const listViewBtn = document.getElementById('listView-btn');
-        const snippetsContainer = document.getElementById('snippets-container');
-        const toggleSnippetsView = (event) => {
-          if (viewProxy.activeView)
-            viewProxy.activeView.classList.remove('active');
-          event.target.classList.add('active');
-          viewProxy.activeView = event.target;
-        };
-        gridViewBtn.addEventListener('click', toggleSnippetsView);
-        listViewBtn.addEventListener('click', toggleSnippetsView);
-
-        // Used to automatically change between the views when a view button is clicked
-        let viewProxy = new Proxy(
-          {
-            activeView: gridViewBtn,
-          },
-          {
-            get(obj, prop) {
-              return prop in obj ? obj[prop] : undefined;
-            },
-            set(obj, prop, value) {
-              snippetsContainer.innerHTML = getSnippetsViewHTML(value.id);
-              obj[prop] = value;
-            },
-          }
-        );
-
-        //Initialize content when page is opened
-        snippetsContainer.innerHTML = getSnippetsViewHTML('gridView-btn');
+        pages.snippetPage.style.display = 'block';
         break;
       case 'settings-btn':
-        const settingsPageHTML = `<h1>Settings</h1>`;
-        MainContent.innerHTML = settingsPageHTML;
+        pages.settingsPage.style.display = 'block';
         break;
     }
   };
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-  // Intialize Page Content On Webview Extension Open
-  changePage('snippets-btn');
-
-  // Used to mimic different Pages - Changes the page each time a button in the sidebar is clicked
-  let pageProxy = new Proxy(
-    {
-      activeBtn: snippetsBtn,
-    },
+  // -=-=-=-=-=-=- Proxies - View Handling -=-=-=-=-=-=-
+  let viewProxy = new Proxy(
+    { activeView: gridViewBtn },
     {
       get(obj, prop) {
         return prop in obj ? obj[prop] : undefined;
       },
       set(obj, prop, value) {
+        snippetsContainer.innerHTML = getSnippetsViewHTML(value.id);
+        obj[prop] = value;
+      },
+    }
+  );
+  let pageProxy = new Proxy(
+    { activeBtn: snippetsBtn },
+    {
+      get(obj, prop) {
+        return prop in obj ? obj[prop] : undefined;
+      },
+      set(obj, prop, value) {
+        for (const page of Object.values(pages)) {
+          page.style.display = 'none';
+        }
+
         changePage(value.id);
         obj[prop] = value;
       },
     }
   );
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-
 
   // Toggle Between active pages
+  const toggleSnippetsView = (event) => {
+    if (viewProxy.activeView) viewProxy.activeView.classList.remove('active');
+    event.target.classList.add('active');
+    viewProxy.activeView = event.target;
+  };
+  gridViewBtn.addEventListener('click', toggleSnippetsView);
+  listViewBtn.addEventListener('click', toggleSnippetsView);
+
   const toggleActiveBtn = (event) => {
     if (pageProxy.activeBtn) pageProxy.activeBtn.classList.remove('active');
     event.target.classList.add('active');
@@ -109,4 +90,7 @@
   };
   settingsBtn.addEventListener('click', toggleActiveBtn);
   snippetsBtn.addEventListener('click', toggleActiveBtn);
+
+  //Initialize Starter Views
+  snippetsContainer.innerHTML = getSnippetsViewHTML('gridView-btn');
 })();
