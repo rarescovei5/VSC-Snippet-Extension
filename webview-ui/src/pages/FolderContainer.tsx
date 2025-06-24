bfd9d8aedb3e764602ca06f8328cca496facb6ba
Covei Rares <rarescovei5@gmail.com>
2025-06-24 20:06:18 +0300

    Dispal something when folder is empty or when search doesn't match anything in folder container

--- webview-ui/src/pages/FolderContainer.tsx
+++ webview-ui/src/pages/FolderContainer.tsx
@@ -5,6 +5,7 @@ import { useParams } from '../ContextRouter/utilities/useParams';
 import type { Prettify, Snippet } from '../types/types';
 import { LocalSnippetCard, RemoteSnippetCard } from '../components/SnippetCard';
 import { gridStyles, QueryContext } from './SnippetsLayout';
+import { VscFiles, VscSearchStop } from 'react-icons/vsc';
 
 const FolderContainer = () => {
   const { selectedLanguage, titleQuery } = React.useContext(QueryContext);
@@ -62,32 +63,55 @@ const FolderContainer = () => {
     }
   }, [fetchRemoteSnippets]);
 
+  const filteredLocalSnippets = React.useMemo(
+    () => localSnippets.filter((snip) => matchesFilter(snip.title, snip.language)),
+    [localSnippets, matchesFilter]
+  );
+  const filteredRemoteSnippets = React.useMemo(
+    () => remoteSnippets.filter((snip) => matchesFilter(snip.title, snip.language)),
+    [remoteSnippets, matchesFilter]
+  );
+
   return (
     <>
       {/* Snippets  */}
       <div style={gridStyles} className="h-full grid gap-1 p-1 overflow-y-auto">
-        {localSnippets
-          .filter((snip) => matchesFilter(snip.title, snip.language))
-          .map((snippet, idx) => (
-            <LocalSnippetCard
-              key={idx}
-              code={snippet.code ?? ''}
-              description={snippet.description ?? ''}
-              language={snippet.language}
-              title={snippet.title}
-            />
-          ))}
-        {remoteSnippets
-          .filter((snip) => matchesFilter(snip.title, snip.language))
-          .map((snippet, idx) => (
-            <RemoteSnippetCard
-              key={idx}
-              code={snippet.code ?? ''}
-              description={snippet.description ?? ''}
-              language={snippet.language}
-              title={snippet.title}
-            />
-          ))}
+        {filteredLocalSnippets.map((snippet, idx) => (
+          <LocalSnippetCard
+            key={idx}
+            code={snippet.code ?? ''}
+            description={snippet.description ?? ''}
+            language={snippet.language}
+            title={snippet.title}
+          />
+        ))}
+        {filteredRemoteSnippets.map((snippet, idx) => (
+          <RemoteSnippetCard
+            key={idx}
+            code={snippet.code ?? ''}
+            description={snippet.description ?? ''}
+            language={snippet.language}
+            title={snippet.title}
+          />
+        ))}
+
+        {!folderData.items.length ? (
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 !text-text flex flex-col items-center text-center">
+            <VscFiles size={48} className="mb-4" />
+            <h1 className="text-2xl font-medium">This Folder is Empty</h1>
+            <p className="text-text-muted mt-1 text-sm">
+              Find snippets on the Discovery page and drag them into this folder from the sidebar to get started.
+            </p>
+          </div>
+        ) : (
+          filteredLocalSnippets.length + filteredRemoteSnippets.length === 0 && (
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 !text-text flex flex-col items-center text-center">
+              <VscSearchStop size={48} className="mb-4" />
+              <h1 className="text-2xl font-medium">No Snippets Matched!</h1>
+              <p className="text-text-muted mt-1 text-sm">Try adjusting your filters</p>
+            </div>
+          )
+        )}
       </div>
     </>
   );
